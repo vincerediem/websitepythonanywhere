6d2c8ea1796e4afdb8936a47c8ad33f47a40de59
Keith <kopenshaw@me.com>
2023-07-01 22:11:01 -0700

    save

--- views.py
+++ views.py
@@ -1,7 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
+
+@views.route('/RSI2')
+def RSI2():
+    return "<h1>RSI2</h1>"
+
+@views.route('/SMA')
+def SMA():
+    return "<h1>SMA</h1>"

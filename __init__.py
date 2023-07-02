0a475ceba7f38f5080e0c6f6f062b5b85abc5247
Keith <kopenshaw@me.com>
2023-07-01 21:36:43 -0700

    simplified, following video now

--- __init__.py
+++ __init__.py
@@ -1,22 +1,17 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from os import path
-from flask_login import LoginManager
-
-db = SQLAlchemy()
-DB_NAME = "database.db"
-
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'skeleton'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    db.init_app(app)
 
     from .views import views
-    #from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
+
+    #from .views import views
+    #from .auth import auth
+
+    #app.register_blueprint(views, url_prefix='/')
     #app.register_blueprint(auth, url_prefix='/')
 
     #from .models import User, Note

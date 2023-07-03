c04e37a3137868afe58c5765ff36fdb43a86798c
Keith <kopenshaw@me.com>
2023-07-03 14:30:11 -0700

    fixed error breaking symbol into characters

--- views.py
+++ views.py
@@ -13,15 +13,17 @@ def home():
 def RSI2():
     if request.method == 'POST': #if a stock is entered, will take you to results page
         stock_list = request.form.get('stock')
-        return redirect(url_for('views.RSI2_result', stock=stock_list))
+        stock_list = stock_list.split()
+        return redirect(url_for('views.RSI2_result', stock_list=','.join(stock_list)))
     else: #else reruns page
         return render_template('RSI2.html')
 
-@views.route('/RSI2/<stock>')
-def RSI2_result(stock):
-    final_balance, initial_balance, stock, positions, trade_gains_losses, positions_sold = rsi2.backtest_strategy(stock)
+@views.route('/RSI2/<stock_list>')
+def RSI2_result(stock_list):
+    stock_list = stock_list.split(',')
+    final_balance, initial_balance, stock, positions, trade_gains_losses, positions_sold = rsi2.backtest_strategy(stock_list)
     final_metrics = rsi2.return_final_metrics(final_balance, initial_balance, stock, positions, trade_gains_losses)
-    return render_template("result.html", stock=stock, positions_sold=positions_sold, final_metrics=final_metrics)
+    return render_template("result.html", stock_list=stock_list, positions_sold=positions_sold, final_metrics=final_metrics)
 
 
 @views.route('/SMA')

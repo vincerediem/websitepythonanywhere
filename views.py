7b8f18c8187fe159557148486d40ec18ded1ed8a
Keith <kopenshaw@me.com>
2023-07-07 14:13:11 -0700

    site now display a df of open positions

--- views.py
+++ views.py
@@ -21,15 +21,16 @@ def RSI2():
 @views.route('/RSI2/<stock_list>')
 def RSI2_result(stock_list):
     stock_list = stock_list.split(',')
-    final_balance, initial_balance, stock, positions, trade_gains_losses, positions_sold = rsi2.backtest_strategy(stock_list)
+    final_balance, initial_balance, stock, positions, trade_gains_losses, positions_sold, open_df = rsi2.backtest_strategy(stock_list)
     final_metrics = rsi2.return_final_metrics(final_balance, initial_balance, stock, positions, trade_gains_losses)
-    trade_metrics, df = rsi2.trade_metrics(stock, positions_sold) #gives list of dics for each trade and df to print
+    trade_metrics, closed_df = rsi2.trade_metrics(stock, positions_sold) #gives list of dics for each trade and df to print
     return render_template("result.html", 
                            stock_list=stock_list, 
-                           positions_sold=positions_sold, 
+                           positions=positions, 
                            final_metrics=final_metrics,
                            trade_metrics=trade_metrics,
-                           df=df.to_html())
+                           closed_df=closed_df.to_html(),
+                           open_df=open_df.to_html())
 
 @views.route('/SMA')
 def SMA():

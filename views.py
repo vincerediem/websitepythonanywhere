3ffa00dff2c85767bc72367de61288a9ec5d993c
Keith <kopenshaw@me.com>
2023-07-14 10:23:23 -0700

    plots graph on site!

--- views.py
+++ views.py
@@ -21,16 +21,25 @@ def RSI2():
 @views.route('/RSI2/<stock_list>')
 def RSI2_result(stock_list):
     stock_list = stock_list.split(',')
-    final_balance, initial_balance, stock, positions, trade_gains_losses, positions_sold, open_df, percent_gains_losses = rsi2.backtest_strategy(stock_list)
+
+    #intial backtest
+    final_balance, initial_balance, stock, positions, trade_gains_losses, positions_sold, open_df, percent_gains_losses, fig = rsi2.backtest_strategy(stock_list)
+
+    #metrics
     final_metrics = rsi2.final_metrics(final_balance, initial_balance, stock, positions, trade_gains_losses, percent_gains_losses)
     trade_metrics, closed_df = rsi2.trade_metrics(stock, positions_sold) #gives list of dics for each trade and df to print
+
+    #converts plotly "fig" to html
+    plot_html = fig.to_html(full_html=False, include_plotlyjs='cdn')
+
     return render_template("result.html", 
                            stock_list=stock_list, 
                            positions=positions, 
                            final_metrics=final_metrics,
                            trade_metrics=trade_metrics,
                            closed_df=closed_df.to_html(),
-                           open_df=open_df.to_html())
+                           open_df=open_df.to_html(),
+                           plot_html=plot_html)
 
 @views.route('/SMA')
 def SMA():
